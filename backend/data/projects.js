8d765a4823cc93fd3b577b21044334d9d8d6d8dd
byronklep <byronklep@gmail.com>
2021-05-17 23:31:05 -0500

    Sample projects.

---
+++ backend/data/projects.js
@@ -0,0 +1,70 @@
+const projects = [
+  {
+    title: 'Crypto Wise',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+    techUsed:
+      'HTML, CSS, JavaScript, React, Node, Bootstrap, CryptoCompare API',
+    category: 'frontend, crypto, React',
+    image1: '/img/projects/crypto/crypto1.jpg',
+    image2: '/img/projects/crypto/crypto2.jpg',
+    image3: '/img/projects/crypto/crypto3.jpg',
+  },
+  {
+    title: 'DeLeon Safety Solutions',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+    techUsed:
+      'HTML, CSS, JavaScript, React, Node, Bootstrap, CryptoCompare API',
+    category: 'frontend, crypto, React',
+    image1: '/img/projects/crypto/crypto1.jpg',
+    image2: '/img/projects/crypto/crypto2.jpg',
+    image3: '/img/projects/crypto/crypto3.jpg',
+  },
+  {
+    title: "Hero's Corner",
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+    techUsed:
+      'HTML, CSS, JavaScript, React, Node, Bootstrap, CryptoCompare API',
+    category: 'frontend, crypto, React',
+    image1: '/img/projects/crypto/crypto1.jpg',
+    image2: '/img/projects/crypto/crypto2.jpg',
+    image3: '/img/projects/crypto/crypto3.jpg',
+  },
+  {
+    title: 'Texas Latino Workers Center',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+    techUsed:
+      'HTML, CSS, JavaScript, React, Node, Bootstrap, CryptoCompare API',
+    category: 'frontend, crypto, React',
+    image1: '/img/projects/crypto/crypto1.jpg',
+    image2: '/img/projects/crypto/crypto2.jpg',
+    image3: '/img/projects/crypto/crypto3.jpg',
+  },
+  {
+    title: 'Fungi API',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+    techUsed:
+      'HTML, CSS, JavaScript, React, Node, Bootstrap, CryptoCompare API',
+    category: 'frontend, crypto, React',
+    image1: '/img/projects/crypto/crypto1.jpg',
+    image2: '/img/projects/crypto/crypto2.jpg',
+    image3: '/img/projects/crypto/crypto3.jpg',
+  },
+  {
+    title: 'Serials',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+    techUsed:
+      'HTML, CSS, JavaScript, React, Node, Bootstrap, CryptoCompare API',
+    category: 'frontend, crypto, React',
+    image1: '/img/projects/crypto/crypto1.jpg',
+    image2: '/img/projects/crypto/crypto2.jpg',
+    image3: '/img/projects/crypto/crypto3.jpg',
+  },
+]
+
+export default projects
